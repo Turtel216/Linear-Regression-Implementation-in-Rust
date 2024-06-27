4da322c58f9f2b87d46673ea03aa0bf224aa25ad
Turtel216 <papadimitrios216@gmail.com>
2024-06-27 09:39:27 +0300

    Implemented train_model method

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,22 @@ this project. I am probably going to refactor
 this code a bunch. Please be gentle ᴖ̈
 
               ＿＿
-　　　　　🌸＞　　フ 
-　　　　　| 　_　 _l   
-　 　　　／` ミ＿xノ  
-　　 　 /　　　 　 |
-　　　 /　 ヽ　　 ﾉ
-　 　 │　　|　|　|
-　／￣|　　 |　|　|
-　| (￣ヽ＿_ヽ_)__)
-　＼二つ
+🌸＞　　フ
+| 　_　 _l
+／` ミ＿xノ
+/　　　 　 |
+/　 ヽ　　 ﾉ
+│　　|　|　|
+／￣|　　 |　|　|
+| (￣ヽ＿_ヽ_)__)
+＼二つ
 
 
 */
 
 struct LinearRegression {
     beta: f32,
-    weight: [f32],
+    weight: Vec<f32>,
     learning_rate: f32,
     epochs: i64,
 }
@@ -29,7 +29,7 @@ impl LinearRegression {
     // Predict on a trained model
     fn predict(&self, inputs: &[f32]) -> Option<f32> {
         if inputs.len() <= 0 {
-            return None
+            return None;
         }
         let mut result: f32 = 0.0;
 
@@ -44,10 +44,38 @@ impl LinearRegression {
     }
 
     // Train the linear regression model
-    fn trainModel(&self, train_data: [f32], result: [32]) -> () {}
+    fn train_model(&mut self, training_data: Vec<Vec<f32>>, result: Vec<f32>) -> () {
+        if training_data.len() <= 0 {
+            // throw error
+        }
+
+        for _e in 0..self.epochs {
+            let mut _mse: f32 = 0.0;
+
+            for i in 0..training_data.len() {
+                let temp_input: &[f32] = &training_data[i];
+
+                let _predicted_value = match self.predict(temp_input) {
+                    Some(prediction) => prediction,
+                    _ => 0.0, //TODO handle error
+                };
+
+                let error = _predicted_value - result[i];
+                _mse = error * error + _mse;
+
+                for j in 0..self.weight.len() {
+                    self.weight[j] = self.weight[j] - self.learning_rate * error * temp_input[j];
+                }
+
+                self.beta = self.beta - self.learning_rate * error;
+            }
+
+            _mse = f32::sqrt(_mse) / training_data.len() as f32;
+        }
+    }
 
     // Function to build a new model
-    fn model(beta: f32, weight: [f32], learning_rate: f32, epochs: i64) -> Self {
+    fn model(beta: f32, weight: Vec<f32>, learning_rate: f32, epochs: i64) -> Self {
         LinearRegression {
             beta,
             weight,
@@ -55,7 +83,6 @@ impl LinearRegression {
             epochs,
         }
     }
-
 }
 
 fn main() {}
